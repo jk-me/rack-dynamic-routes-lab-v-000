28347e51acff3a1a6d12a10f155d3b182aec2605
Jenny K <jkam17@gmail.com>
2018-09-25 03:20:04 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,22 @@
+require_relative '/item.rb'
+
+class Application
+  @@items =[]
+  def call(env)
+    req = Rack::Response(env)
+
+    if req.path.match(/items/)
+      i = req.path.split('/items/').last 
+      a = @@items.find{|x| x.name == i}
+      if a != []
+        resp.write "#{a.price}"
+      else 
+        resp.write "Item not found"
+        resp.status 400
+      end 
+    else
+      resp.write('Route not found') 
+      resp.status = 404
+    end
+  end
+end
