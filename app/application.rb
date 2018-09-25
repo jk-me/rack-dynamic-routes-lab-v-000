bbdf9225a453d37d51e1918a7157ba7a294066fa
Jenny K <jkam17@gmail.com>
2018-09-25 03:29:53 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
     if req.path.match(/items/)
       i = req.path.split('/items/').last
       a = @@items.find{|x| x.name == i}
-      
+
       if a != []
         resp.write "#{a}"
       else
